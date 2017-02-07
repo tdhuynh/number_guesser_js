3b5f35ead4255621c99b6040279d66a9dd4bec95
tdhuynh <tommyhuynh93@gmail.com>
2017-02-07 15:11:13 -0500

    number guesser game in javascript

---
+++ practice.js
@@ -0,0 +1,36 @@
+console.log('Hello world');
+
+function sayHello() {
+  alert("hello world")
+}
+
+var name = "Tommy";
+var money = 5000.00;
+
+var a = 10; var b = 20;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(b % a);
+console.log(a--);
+console.log(a++);
+
+var age = 23;
+if (age > 21){
+  console.log("Old enough to drink")
+}
+else {
+  console.log("Don't sell, it's a minor")
+}
+
+
+var grade='F';
+switch (grade){
+  case 'A': console.log("a"); break;
+  case 'B': console.log("b"); break;
+  case 'C': console.log("c"); break;
+  case 'D': console.log("d"); break;
+  case 'F': console.log("f"); break;
+}
